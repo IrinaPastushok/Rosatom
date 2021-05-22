3e768a404b068cb57fdac7cfbe43b2da7a312c1f
IrinaPastushok <84584434+IrinaPastushok@users.noreply.github.com>
2021-05-22 17:03:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,67 +1,43 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 using System.Threading;
-
-namespace kahaton3
+public class Change : MonoBehaviour
 {
-class Program
-{
-public sealed class DateAndTime { }
-static int GetRandom(int a, int b) //функция дающая случайное значение
-{
-//Создание объекта для генерации чисел (с указанием начального значения)
-Random rnd = new Random();
+    public Sprite[] ImgList;
+    SpriteRenderer Sprite;
+    public int IndexImg = 1;
+    public int temp = 25;
+    public int n;
+    public int b;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //IndexImg = Random.Range(0, 3);
+        Sprite = GetComponent<SpriteRenderer>();
+    }
 
-//Получить случайное число
-int value = rnd.Next(a, b);
-
-//Вернуть полученное значение
-return value;
-}
-
-static void Main(string[] args)
-{
-Console.WriteLine("test");
-int r = GetRandom(-30, 40); // случайная начальная темпа
-int a = 0; // Некое число от которого зависит колебание температуры
-int e = 0; // число "отвечает чтобы данные передовались не каждые 0,5 сек, потому что температура так не скачит, а каждые 5 сек
-int colour = 0; // цвет нашей комнаты
-int time = 0; // переменная для времени
-int light = 0; // яркость света нашей комнаты
-
-while (true)
-{
-a = GetRandom(-3, 3);
-r += a;
-if (r > 40) r = 40;
-if (r < -30) r = 30;
-Console.WriteLine("{0}", r); // ну чисто посмотреть что там, интереса ради
-Thread.Sleep(100); //Sleep(500);// здесь указано время задержки в миллисекундах, тоесть наша задержка 0,5 сек, потому что время отклика 0,5 по заданию
-
-e++;
-if (e == 10)
-{ // когда пройдет 5 секунд произойдет проверка цвета нашего помещения и проверка времени
-if (r > 20) { colour = 1; Console.WriteLine("red\n\n"); }
-else if (r > 0) { colour = 2; Console.WriteLine("yellow\n\n"); }
-else { colour = 3; Console.WriteLine("blue\n\n"); }
-e = 0;
-colour = 0;
-
-
-DateTime dt = new DateTime(); // работа со временем дня
-dt = DateTime.Now;
-time = dt.Hour;
-if (time > 18) { Console.WriteLine("evening\n\n"); }
-else if (time > 12) { Console.WriteLine("afternoon\n\n"); }
-else if (time > 6) { Console.WriteLine("morning\n\n"); }
-else { Console.WriteLine("night\n\n"); }
-}
-
-}
-}
-}
-
+    // Update is called once per frame
+    void Update()
+    {
+        n = Random.Range(0, 10);
+        if (n < 2)
+        {
+            b = Random.Range(-1, 2);
+            if (b == -1)
+            {
+                temp--;
+            }
+            if (b == 1)
+            {
+                temp++;
+            }
+        }
+        //IndexImg = Random.Range(0, 3);
+        if (temp >= 30) IndexImg = 2;
+        if ((temp > 15) && (temp < 30)) IndexImg = 1;
+        if (temp <= 15) IndexImg = 0;
+        Thread.Sleep(100);
+        Sprite.sprite = ImgList[IndexImg];
+    }
 }
